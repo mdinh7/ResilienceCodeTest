46d29945a10294bafb3ad476fbe9ce553e67afc2
Mendel Dinh <mdinh6@gmail.com>
2021-09-11 03:47:20 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import vessel from './vessel.js'
---
+++ src/vessel.js
@@ -0,0 +1,22 @@
+class vessel{
+    constructor(id) {
+        this.id = id;
+    }
+
+// function to fetch vessel id
+
+// function to fetch vessel state
+
+// update UI table with most up to date vessel data
+
+// function to fetch state of input valve
+
+// function to change state of input valve
+
+// function to fetch state of output valve
+
+// function to change state of output valve
+
+}
+
+export default vessel;
