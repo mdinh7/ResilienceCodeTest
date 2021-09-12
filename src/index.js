c071a555436318ccba6daa695a976cfe6bdd2f90
Mendel Dinh <mdinh6@gmail.com>
2021-09-11 21:59:38 -0700

    request functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import vessel from './vessel.js'
+import Vessel from './vessel.js'
+
+document.getElementById('getID').addEventListener('click', function(){
+    var vessel = new Vessel
+    vessel.getVessel().then(data => {
+        vessel.getVesselStats(data)
+        vessel.getInputValve(data)
+        vessel.getOutputValve(data)
+    })
+});
+
+document.getElementById('getID2').addEventListener('click', function(){
+    let vesselStats = vessel.getVesselStats(vesselID)
+    let IVState = vessel.getInputValve(vesselID)
+    let OVState = vessel.getOutputValve(vesselID)
+});
--- src/vessel.js
+++ src/vessel.js
@@ -1,22 +1,117 @@
-class vessel{
-    constructor(id) {
-        this.id = id;
+class Vessel{
+    
+    // function to fetch vessel id
+    async getVessel(){
+        let vesselID;
+
+        const testHeaders = new Headers();
+        testHeaders.append('Content-Type', 'application/json');
+        testHeaders.append('Accept', 'application/json');
+        testHeaders.append('Access-Control-Allow-Origin', '*');
+
+        const vesselRequest = new Request('http://mini-mes.resilience.com/bioreactor/0', {
+            method: 'GET',
+            headers: testHeaders,
+            mode: 'cors',
+            cache: 'default',
+        });
+
+        vesselID = await fetch(vesselRequest)
+        .then(response => response.json())
+        .then(data => {
+            return data
+        })
+
+        return vesselID.id
+    }   
+
+    // function to fetch vessel state
+    async getVesselStats(vesselID){
+        let vesselStats;
+
+        const testHeaders = new Headers();
+        testHeaders.append('Content-Type', 'application/json');
+        testHeaders.append('Accept', 'application/json');
+
+        const vesselStatsRequest = new Request('http://mini-mes.resilience.com/bioreactor/' + vesselID, {
+            method: 'GET',
+            headers: testHeaders,
+            mode: 'cors',
+            cache: 'default',
+        });
+
+            vesselStats = await fetch(vesselStatsRequest)
+            .then(response => response.json())
+            .then(data => {
+                return data
+            });
+
+            return vesselStats
+
     }
 
-// function to fetch vessel id
+    // update UI table with most up to date vessel data
+    updateStatsUI(){
 
-// function to fetch vessel state
+    }
 
-// update UI table with most up to date vessel data
+    // function to fetch state of input valve
+    async getInputValve(vesselID){
+        let vesselIVState;
+        const testHeaders = new Headers();
+        testHeaders.append('Content-Type', 'application/json');
+        testHeaders.append('Accept', 'application/json');
 
-// function to fetch state of input valve
+        const vesselIVRequest = new Request('http://mini-mes.resilience.com/bioreactor/' + vesselID + '/input-valve', {
+            method: 'GET',
+            headers: testHeaders,
+            mode: 'cors',
+            cache: 'default',
+        });
 
-// function to change state of input valve
+            vesselIVState = await fetch(vesselIVRequest)
+            .then(response => response.json())
+            .then(data => {
+                return data
+            });
 
-// function to fetch state of output valve
+            return vesselIVState.state
+    }
 
-// function to change state of output valve
+    // function to change state of input valve
+    putInputValve(vesselID){
+
+    }
+
+    // function to fetch state of output valve
+    async getOutputValve(vesselID){
+        let vesselOVState;
+        const testHeaders = new Headers();
+        testHeaders.append('Content-Type', 'application/json');
+        testHeaders.append('Accept', 'application/json');
+
+        const vesselOVRequest = new Request('http://mini-mes.resilience.com/bioreactor/' + vesselID + '/output-valve', {
+            method: 'GET',
+            headers: testHeaders,
+            mode: 'cors',
+            cache: 'default',
+        });
+
+            vesselOVState = await fetch(vesselOVRequest)
+            .then(response => response.json())
+            .then(data => {
+                return data
+            });
+
+            return vesselOVState.state
+
+    }
+
+    // function to change state of output valve
+    putOutputValve(vesselID){
+
+    }
 
 }
 
-export default vessel;
+export default Vessel;
